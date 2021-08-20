7339fe5ef7549a4b437b74e4da156f44890daf98
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2021-08-19 21:49:27 -0300

    s5a51 end

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -11,13 +11,17 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspicionTime = 5f;
 
         private GameObject player;
         private Fighter fighter;
+        private Mover mover;
 
         #region AI Memory
         private Vector3 guardPosition;
+        private float timeSinceLastSawPlayer = Mathf.Infinity; 
         #endregion
+
         private bool InAttackRangeOfPlayer
         {
             get
@@ -30,21 +34,43 @@ namespace RPG.Control
         {
             player = GameObject.FindWithTag("Player");
             fighter = GetComponent<Fighter>();
+            mover = GetComponent<Mover>();
 
             guardPosition = transform.position;
         }
 
         private void Update()
         {
+            UpdateTimers();
+
             if (GetComponent<Health>().IsDead) return;
-            if (InteractWithCombat()) return;
-            if (InteractWithMovement()) return;
+            if (BehaveInCombat()) return;
+            if (BehaveInSuspicion()) return;
+            if (BehaveInGuard()) return;
         }
 
-        private bool InteractWithCombat()
+        private void UpdateTimers()
+        {
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private bool BehaveInSuspicion()
+        {
+            if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                // be suspicious
+                GetComponent<ActionScheduler>().CancelCurrentAction();
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool BehaveInCombat()
         {
             if (InAttackRangeOfPlayer && fighter.CanAttack(player))
             {
+                timeSinceLastSawPlayer = 0;
                 fighter.Attack(player);
                 return true;
             }
@@ -52,9 +78,9 @@ namespace RPG.Control
             return false;
         }
 
-        private bool InteractWithMovement()
+        private bool BehaveInGuard()
         {
-            GetComponent<Mover>().StartMoveAction(guardPosition);
+            mover.StartMoveAction(guardPosition);
             return true;
         }
 
